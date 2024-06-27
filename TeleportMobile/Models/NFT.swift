5f72f702e2c27a59fc8c8b4b0b43b1f01732a124
Rodney Gainous Jr <rodney.gainous@gmail.com>
2024-06-26 21:07:06 -0700

    Add description

--- TeleportMobile/Models/NFT.swift
+++ TeleportMobile/Models/NFT.swift
@@ -5,10 +5,9 @@
 //  Created by Rodney Gainous Jr on 6/26/24.
 //
 
-import Foundation
-
 struct NFT: Identifiable, Codable {
     let id: String
     let name: String
     let imageURL: String
+    let description: String
 }
