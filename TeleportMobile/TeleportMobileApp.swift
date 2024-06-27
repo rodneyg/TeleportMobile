83f716404a1412473f1c6aacd9ed3e677e5e28d8
Rodney Gainous Jr <rodney.gainous@gmail.com>
2024-06-26 19:51:11 -0700

    Initial Commit

---
+++ TeleportMobile/TeleportMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeleportMobileApp.swift
+//  TeleportMobile
+//
+//  Created by Rodney Gainous Jr on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TeleportMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TeleportMobileUITests/TeleportMobileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TeleportMobileUITestsLaunchTests.swift
+//  TeleportMobileUITests
+//
+//  Created by Rodney Gainous Jr on 6/26/24.
+//
+
+import XCTest
+
+final class TeleportMobileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
