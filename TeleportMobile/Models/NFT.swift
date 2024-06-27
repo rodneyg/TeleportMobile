bc2abc99151dcf992c2338406d3b51979c1f4fcc
Rodney Gainous Jr <rodney.gainous@gmail.com>
2024-06-26 20:02:52 -0700

    Add NFT model
    Create NFT struct with id, name, and imageURL properties

---
+++ TeleportMobile/Models/NFT.swift
@@ -0,0 +1,14 @@
+//
+//  NFT.swift
+//  TeleportMobile
+//
+//  Created by Rodney Gainous Jr on 6/26/24.
+//
+
+import Foundation
+
+struct NFT: Identifiable, Codable {
+    let id: String
+    let name: String
+    let imageURL: String
+}
